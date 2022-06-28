f0f457547305d380bc7550a3122fa46fc63ed128
Nick Messer <messer185@gmail.com>
2022-06-27 21:12:08 -0400

    Zombie loot stuff, recipes, and the first advancement.

--- src/main/java/net/messer/mystical_index/event/LootTableEvent.java
+++ src/main/java/net/messer/mystical_index/event/LootTableEvent.java
@@ -1,12 +1,10 @@
 package net.messer.mystical_index.event;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.messer.mystical_index.item.ModItems;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
 
@@ -15,8 +13,10 @@ public class LootTableEvent {
 
     public static void registerLootTable(){
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if(source.isBuiltin() && ZOMBIE_LOOT_TABLE_ID.equals(id)){
-                LootPool.Builder poolBuilder = LootPool.builder().with(ItemEntry.builder(ModItems.EMPTY_PAGE));
+            if(ZOMBIE_LOOT_TABLE_ID.equals(id)){
+                LootPool.Builder poolBuilder = LootPool.builder()
+                                .with(ItemEntry.builder(ModItems.EMPTY_PAGE))
+                                .conditionally(RandomChanceLootCondition.builder(0.1f));
                 tableBuilder.pool(poolBuilder);
             }
         });
