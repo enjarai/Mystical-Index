ddc96e467fa887a62d23093eca20947e904b869a
enjarai <enjarai123@gmail.com>
2023-10-14 16:02:18 +0200

    Pain and suffering

--- src/main/java/net/messer/mystical_index/client/render/ChiseledBookshelfBlockEntityRenderer.java
+++ src/main/java/net/messer/mystical_index/client/render/ChiseledBookshelfBlockEntityRenderer.java
@@ -1,13 +1,17 @@
 package net.messer.mystical_index.client.render;
 
-import net.messer.mystical_index.util.BigStack;
+import net.messer.mystical_index.item.custom.book.MysticalBookItem;
+import net.messer.mystical_index.item.custom.page.type.ItemStorageTypePage;
+import net.messer.mystical_index.mixin.accessor.ChiseledBookshelfBlockInvoker;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
-
-import java.util.List;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.RotationAxis;
 
 public class ChiseledBookshelfBlockEntityRenderer implements BlockEntityRenderer<ChiseledBookshelfBlockEntity> {
     private final ItemCirclesRenderer circleRenderer = new ItemCirclesRenderer(true);
@@ -17,20 +21,34 @@ public class ChiseledBookshelfBlockEntityRenderer implements BlockEntityRenderer
 
     @Override
     public void render(ChiseledBookshelfBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        matrices.scale(-0.01f, -0.01f, -0.01f);
-        circleRenderer.render(matrices, 0, 0, 0, List.of(
-                new BigStack(entity.getStack(0)),
-                new BigStack(entity.getStack(1))
-        ));
+        var hitResult = MinecraftClient.getInstance().crosshairTarget;
 
+        if (hitResult instanceof BlockHitResult blockHitResult && entity.getPos().equals(blockHitResult.getBlockPos())) {
 
-//        var size = 4;
-//        var matrix = matrices.peek().getPositionMatrix();
-//        var color = 0xffffffff;
-//        var vertices = vertexConsumers.getBuffer(RenderLayer.getText(MysticalIndex.id("textures/gui/circle_24.png")));
-//        vertices.vertex(matrix, -size, -size, 0).color(color).texture(0, 0).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).next();
-//        vertices.vertex(matrix, -size, size, 0).color(color).texture(0, 1).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).next();
-//        vertices.vertex(matrix, size, size, 0).color(color).texture(1, 1).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).next();
-//        vertices.vertex(matrix, size, -size, 0).color(color).texture(1, 0).light(LightmapTextureManager.MAX_LIGHT_COORDINATE).next();
+            var facing = entity.getCachedState().get(HorizontalFacingBlock.FACING);
+            ChiseledBookshelfBlockInvoker.invokeGetHitPos(blockHitResult, facing).ifPresent(hitPos -> {
+
+                var slot = ChiseledBookshelfBlockInvoker.invokeGetSlotForHitPos(hitPos);
+                var bookStack = entity.getStack(slot);
+                var book = bookStack.getItem();
+
+                if (book instanceof MysticalBookItem bookItem && bookItem.getTypePage(bookStack) instanceof ItemStorageTypePage storagePage) {
+                    var stacks = storagePage.getContents(bookStack).getAll();
+                    var x = (slot % 3 / 3f + 1f / 6f) / 16f * 15f + 1f / 32f;
+                    var y = slot / 3 / 2f + 1f / 4f;
+
+                    matrices.push();
+
+                    matrices.translate(1.0f - x, 1.0f - y, 0f);
+//                    matrices.translate(0.5f, 0f, 0.5f);
+//                    matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180 + facing.asRotation()));
+//                    matrices.translate(-0.5f, 0f, -0.5f);
+                    matrices.scale(0.01f, 0.01f, 0.01f);
+                    circleRenderer.render(matrices, 0, 0, 0, 0xF000F0, stacks);
+
+                    matrices.pop();
+                }
+            });
+        }
     }
 }
--- src/main/java/net/messer/mystical_index/client/render/ItemCirclesRenderer.java
+++ src/main/java/net/messer/mystical_index/client/render/ItemCirclesRenderer.java
@@ -2,19 +2,33 @@ package net.messer.mystical_index.client.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.messer.mystical_index.MysticalIndex;
+import net.messer.mystical_index.mixin.accessor.DrawContextInvoker;
 import net.messer.mystical_index.util.BigStack;
 import net.messer.mystical_index.util.MathUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.crash.CrashCallable;
+import net.minecraft.util.crash.CrashException;
+import net.minecraft.util.crash.CrashReport;
+import net.minecraft.util.crash.CrashReportSection;
+import net.minecraft.util.math.RotationAxis;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.joml.Quaternionf;
 
 import java.util.List;
 import java.util.Map;
 
 public class ItemCirclesRenderer {
+    private static final Quaternionf ITEM_LIGHT_ROTATION_3D = RotationAxis.POSITIVE_X.rotationDegrees(-15).mul(RotationAxis.POSITIVE_Y.rotationDegrees(15));
+    private static final Quaternionf ITEM_LIGHT_ROTATION_FLAT = RotationAxis.POSITIVE_X.rotationDegrees(-45);
     public static final Identifier CIRCLES_TEXTURE = MysticalIndex.id("textures/gui/circles.png");
     public static final int CIRCLE_TEXTURES_SIZE = 256;
     public static final Map<Integer, Identifier> CIRCLE_TEXTURES = Map.of(
@@ -30,21 +44,29 @@ public class ItemCirclesRenderer {
         this.inWorld = inWorld;
     }
 
-    public void render(MatrixStack matrices, double x, double y, double z, List<BigStack> stacks) {
-        var drawContext = new DrawContext(
-                MinecraftClient.getInstance(),
-                matrices,
-                VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer())
+    public void render(MatrixStack matrices, double x, double y, double z, int light, List<BigStack> stacks) {
+        matrices.push();
+        matrices.multiply(RotationAxis.NEGATIVE_Z.rotationDegrees(180));
+//        matrices.multiplyPositionMatrix(new Matrix4f().scale(1, -1, 1));
+
+        var drawContext = DrawContextInvoker.constructor(
+                MinecraftClient.getInstance(), matrices,
+                MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers()
         );
-        render(drawContext, x, y, z, stacks);
+        render(drawContext, x, y, z, light, stacks);
+
+        matrices.pop();
     }
 
     public void render(DrawContext context, double x, double y, List<BigStack> stacks) {
-        render(context, x, y, 0, stacks);
+        render(context, x, y, 0, 0xF000F0, stacks);
     }
 
-    public void render(DrawContext context, double x, double y, double z, List<BigStack> stacks) {
+    public void render(DrawContext context, double x, double y, double z, int light, List<BigStack> stacks) {
         if (stacks.isEmpty()) return;
+
+        var matrices = context.getMatrices();
+        matrices.push();
 
         var primary = stacks.get(0);
         var secondary = stacks.size() > 1 ?
@@ -52,13 +74,15 @@ public class ItemCirclesRenderer {
         var ternary = stacks.size() > SECONDARY_CIRCLE_ITEM_COUNT ?
                 stacks.subList(SECONDARY_CIRCLE_ITEM_COUNT, Math.min(TERNARY_CIRCLE_ITEM_COUNT, stacks.size())) : null;
 
-        if (secondary != null) drawItemCircle(context, x, y, z, 24, secondary);
-        if (ternary != null) drawItemCircle(context, x, y, z, 48, ternary);
+        if (secondary != null) drawItemCircle(context, x, y, z, light, 24, secondary);
+        if (ternary != null) drawItemCircle(context, x, y, z, light, 48, ternary);
         drawItemCircle(context, x, y, z, true);
-        drawItem(context, x, y, z, primary);
+        drawItemStack(context, x, y, z, light, primary);
+
+        matrices.pop();
     }
 
-    private void drawItemCircle(DrawContext context, double x, double y, double z, int radius, List<BigStack> items) {
+    private void drawItemCircle(DrawContext context, double x, double y, double z, int light, int radius, List<BigStack> items) {
         var itemCount = items.size();
         var circleTexture = CIRCLE_TEXTURES.get(radius);
 
@@ -79,7 +103,7 @@ public class ItemCirclesRenderer {
             var itemY = (int) y + (radius * Math.sin(offset));
 
             drawItemCircle(context, itemX, itemY, z, false);
-            drawItem(context, itemX, itemY, z, stack);
+            drawItemStack(context, itemX, itemY, z, light, stack);
         }
     }
 
@@ -95,19 +119,76 @@ public class ItemCirclesRenderer {
         matrices.pop();
     }
 
-    protected void drawItem(DrawContext context, double x, double y, double z, BigStack stack) {
+    protected void drawItemStack(DrawContext context, double x, double y, double z, int light, BigStack stack) {
         var count = stack.getAmount();
         var matrices = context.getMatrices();
 
-        matrices.push();
+        if (inWorld) {
+            matrices.push();
+            matrices.translate(0, 0, -1);
+            matrices.multiplyPositionMatrix(new Matrix4f().scale(1, 1, 0.01f));
 
-        if (inWorld) matrices.translate(0, 0, -150);
-        context.drawItem(stack.getItemStack(), (int) x - 8, (int) y - 8);
+            drawItem(matrices, context.getVertexConsumers(), stack.getItemStack(), (int) x, (int) y, light);
+            matrices.pop();
+        } else {
+//        matrices.push();
+//
+//            matrices.translate(0, 0, -202);
+//            matrices.multiplyPositionMatrix(new Matrix4f().scale(1, 1, -1));
+            context.drawItem(stack.getItemStack(), (int) x - 8, (int) y - 8);
 
-        if (inWorld) matrices.translate(0, 0, -48);
-        context.drawItemInSlot(MinecraftClient.getInstance().textRenderer, stack.getItemStack(), (int) x - 8, (int) y - 8,
-                count > 1 ? MathUtil.shortNumberFormat(count) : "0");
+            context.drawItemInSlot(MinecraftClient.getInstance().textRenderer, stack.getItemStack(), (int) x - 8, (int) y - 8,
+                    count > 1 ? MathUtil.shortNumberFormat(count) : "");
+//        matrices.pop();
+        }
+    }
 
-        matrices.pop();
+    private void drawItem(MatrixStack matrices, VertexConsumerProvider vertexConsumers, ItemStack stack, int x, int y, int light) {
+        if (!stack.isEmpty()) {
+            var itemRenderer = MinecraftClient.getInstance().getItemRenderer();
+            var bakedModel = itemRenderer.getModel(stack, null, null, 0);
+            matrices.push();
+            matrices.translate(x, y, inWorld ? 0 : 150);
+            if (inWorld) {
+                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180));
+//                matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180));
+            }
+
+            try {
+                if (!inWorld) {
+                    matrices.multiplyPositionMatrix(new Matrix4f().scaling(1.0F, -1.0F, 1.0F));
+                }
+//                matrices.multiplyPositionMatrix(new Matrix4f().scaling(1.0F, 1.0F, 1.0F));
+                matrices.scale(16.0F, 16.0F, 16.0F);
+
+                boolean sideLit = bakedModel.isSideLit();
+                if (!inWorld) {
+                    if (!sideLit) DiffuseLighting.disableGuiDepthLighting();
+                } else {
+                    // Stolen from Extended Drawers, who in turn stole it from Storage Drawers
+                    if (sideLit) {
+                        matrices.peek().getNormalMatrix().mul(new Matrix3f().set(ITEM_LIGHT_ROTATION_3D));
+                    } else {
+                        matrices.peek().getNormalMatrix().mul(new Matrix3f().set(ITEM_LIGHT_ROTATION_FLAT));
+                    }
+                }
+
+                itemRenderer.renderItem(stack, ModelTransformationMode.GUI, false, matrices, vertexConsumers, light, OverlayTexture.DEFAULT_UV, bakedModel);
+
+                if (!inWorld) {
+                    if (!sideLit) DiffuseLighting.enableGuiDepthLighting();
+                }
+            } catch (Throwable e) {
+                CrashReport crashReport = CrashReport.create(e, "Rendering item");
+                CrashReportSection crashReportSection = crashReport.addElement("Item being rendered");
+                crashReportSection.add("Item Type", () -> String.valueOf(stack.getItem()));
+                crashReportSection.add("Item Damage", () -> String.valueOf(stack.getDamage()));
+                crashReportSection.add("Item NBT", () -> String.valueOf(stack.getNbt()));
+                crashReportSection.add("Item Foil", () -> String.valueOf(stack.hasGlint()));
+                throw new CrashException(crashReport);
+            }
+
+            matrices.pop();
+        }
     }
 }
